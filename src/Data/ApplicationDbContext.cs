02646868ea1fde8ea4e1d26b6fb742446e42b3b4
Mandeep <mandeep.shergill@triosstudent.com>
2024-03-08 22:31:38 -0500

    Sleek_Project

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SleekClothing.Models;
+
+namespace SleekClothing.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<UserCart> UserCarts { get; set; }
+        public DbSet<UserWishlist> UserWishlists { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
